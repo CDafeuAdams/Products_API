6123f5ac76911e79a2b99d28a6f740728d630985
CDafeuAdams <117130837+CDafeuAdams@users.noreply.github.com>
2023-02-16 17:50:20 -0600

    create the api view for postman and also created the serializers for the products. I also created the path for urls to both be connected.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import ProductSerializer
+from .models import Product
+
+@api_view(['GET', 'POST'])
+def all_products(request):
+    if request.method == "GET":
+        products_list = Product.objects.all()
+        serializer = ProductSerializer(products_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer .is_valid(raise_exception=True)
+        serializer .save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
